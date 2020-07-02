a7822dd1787254e3335cc9861a1bc7794c0c7a71
LuruichaoZxy <18838813080@163.com>
2020-07-02 15:26:36 +0800

    解决pom报错

--- studyOnline_service/xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ studyOnline_service/xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -10,7 +10,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(value="cms页面管理接口",description = "cms页面管理接口，提供页面的增、删、改、查")
 public interface CmsPageControllerApi {
     //页面查询
-    @ApiOperation("分页查询页面列表")
+    @ApiOperation("分页查询页面列表 ")
     @ApiImplicitParams({
             @ApiImplicitParam(name="page",value = "页码",required=true,paramType="path",dataType="int"),
             @ApiImplicitParam(name="size",value = "每页记录数",required=true,paramType="path",dataType="int")
